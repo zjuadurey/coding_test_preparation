de558045e049525e439626c08d4ed8d47e824406
adurey_laptop_lenove_yoga_14c <1721966349@qq.com>
2024-04-15 22:04:11 +0800

    11

--- sunnywhy/chap2/2.2/11.cpp
+++ sunnywhy/chap2/2.2/11.cpp
@@ -1,5 +1,8 @@
-#include <cstdio>
+#include<cstdio>
 
 int main() {
-    
+    int year, month, day, hour, minute, second;
+    scanf("%d-%d-%d %d:%d:%d", &year, &month, &day, &hour, &minute, &second);
+    printf("%d\n%02d\n%02d\n%02d\n%02d\n%02d", year, month, day, hour, minute, second);
+    return 0;
 }
